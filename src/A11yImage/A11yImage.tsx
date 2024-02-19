5e34b4c04e6bf0c69bb9967722c2a32d6bc6eb43
NgocHieu2008 <tnngochieu2008@gmail.com>
2024-02-19 20:05:15 +0700

    accessible image component 1.0.2

---
+++ src/A11yImage/A11yImage.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+
+export interface ImageProps {
+    /**
+     * Đường dẫn đến hình ảnh.
+     */
+    src: NonEmptyString;
+  
+    /**
+     * Văn bản thay thế mô tả nội dung hình ảnh. (Bắt buộc)
+     */
+    alt: NonEmptyString;
+  
+    /**
+     * Chú thích cho hình ảnh. (Tùy chọn)
+     */
+    caption?: string;
+  
+    /**
+     * Chiều rộng của hình ảnh. (Tùy chọn)
+     */
+    width?: number | string;
+  
+    /**
+     * Chiều cao của hình ảnh. (Tùy chọn)
+     */
+    height?: number | string;
+  
+    /**
+     * Thuộc tính aria-label cung cấp thêm thông tin về hình ảnh cho người dùng sử dụng trình đọc màn hình. (Tùy chọn)
+     */
+    ariaLabel?: string;
+  
+    /**
+     * Xác định xem hình ảnh có phải là hình ảnh trang trí hay không. (Tùy chọn)
+     */
+    decorative?: boolean;
+}
+
+type NonEmptyString = string & { _brand: "NonEmptyString" };
+
+function createNonEmptyString(value: string): NonEmptyString {
+  if (value === "") {
+    throw new Error("Value cannot be empty");
+  }
+  return value as NonEmptyString;
+}
+
+const AccessibleImage = (props:ImageProps) => {
+    const {src, alt, width, height, ariaLabel, decorative, caption, ...otherProps} = props
+    return (
+      <figure>
+        <img
+          src={src}
+          alt={alt}
+          width={width}
+          height={height}
+          aria-label={ariaLabel}
+          aria-hidden={decorative ? "true" : undefined}
+        />
+        {caption && <figcaption>{caption}</figcaption>}
+      </figure>
+    );
+  };
+  
+  export default AccessibleImage;
